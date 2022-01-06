ba8925961dcd48b54442c978992f0c2913faec76
Vlad-Lab319 <solo15mag@gmail.com>
2022-01-06 05:21:54 +0000

    Add injections prevented

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,10 +15,10 @@ FROM assistance_requests
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`, [`%${cohort}%`])
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort} was assisted by teacher ${teacher.teacher}`);
